cdc5af267a8fd20480f99adb400a9507161245f5
Oscar <112303520@cc.ncu.edu.tw>
2023-11-08 19:25:11 +0800

    update

--- hw_2/main.js
+++ hw_2/main.js
@@ -1,66 +1,101 @@
 let myGraph2 = document.getElementById('myGraph2');
 let hw2_trace1 ={};
 hw2_trace1.type ="bar";
-hw2_trace1.name ="第一產業（農、林、漁、牧業）";
+hw2_trace1.name ="惡性腫瘤";
 hw2_trace1.x =[];
 hw2_trace1.y =[];
 
-hw2_trace1.x[0] = "中國的職業人口分布";
-hw2_trace1.y[0] = 中國的職業人口[0]['count'];
-hw2_trace1.x[1] = "日本的職業人口分布";
-hw2_trace1.y[1] = 日本的職業人口[0]['count'];
-hw2_trace1.x[2] = "南韓的職業人口分布";
-hw2_trace1.y[2] = 南韓的職業人口[0]['count'];
-hw2_trace1.x[3] = "臺灣的職業人口分布";
-hw2_trace1.y[3] = 臺灣的職業人口[0]['count'];
 
-hw2_trace1.text = hw2_trace1.y;
-hw2_trace1.textfont = {
-    color: 'white',
-    size:20
-};
+hw2_trace1.x[0] = "臺北市";
+hw2_trace1.y[0] = 臺北市死因統計[0]['count'];
+hw2_trace1.x[1] = "新北市";
+hw2_trace1.y[1] = 新北市死因統計[0]['count'];
+hw2_trace1.x[2] = "桃園市";
+hw2_trace1.y[2] = 桃園市死因統計[0]['count'];
+hw2_trace1.x[3] = "臺中市";
+hw2_trace1.y[3] = 臺中市死因統計[0]['count'];
+hw2_trace1.x[4] = "臺南市";
+hw2_trace1.y[4] = 臺南市死因統計[0]['count'];
+hw2_trace1.x[5] = "高雄市";
+hw2_trace1.y[5] = 高雄市死因統計[0]['count'];
+
+// hw2_trace1.text = hw2_trace1.y;
+//hw2_trace1.textposition = "outside";
+//hw2_trace1.marker = 
+//{
+  //  size:1
+//}
+// hw2_trace1.textfont = {
+//     color: 'black',
+    size:20 
+// };
 
 let hw2_trace2 ={};
 hw2_trace2.type ="bar";
-hw2_trace2.name ="第二產業（工業製造業";
+hw2_trace2.name ="心臟疾病";
 hw2_trace2.x =[];
 hw2_trace2.y =[];
 
-hw2_trace2.x[0] = "中國的職業人口分布";
-hw2_trace2.y[0] = 中國的職業人口[1]['count'];
-hw2_trace2.x[1] = "日本的職業人口分布";
-hw2_trace2.y[1] = 日本的職業人口[1]['count'];
-hw2_trace2.x[2] = "南韓的職業人口分布";
-hw2_trace2.y[2] = 南韓的職業人口[1]['count'];
-hw2_trace2.x[3] = "臺灣的職業人口分布";
-hw2_trace2.y[3] = 臺灣的職業人口[1]['count'];
+hw2_trace2.x[0] = "臺北市";
+hw2_trace2.y[0] = 臺北市死因統計[1]['count'];
+hw2_trace2.x[1] = "新北市";
+hw2_trace2.y[1] = 新北市死因統計[1]['count'];
+hw2_trace2.x[2] = "桃園市";
+hw2_trace2.y[2] = 桃園市死因統計[1]['count'];
+hw2_trace2.x[3] = "臺中市";
+hw2_trace2.y[3] = 臺中市死因統計[1]['count'];
+hw2_trace2.x[4] = "臺南市";
+hw2_trace2.y[4] = 臺南市死因統計[1]['count'];
+hw2_trace2.x[5] = "高雄市";
+hw2_trace2.y[5] = 高雄市死因統計[1]['count'];
 
-hw2_trace2.text = hw2_trace2.y;
-hw2_trace2.textfont = {
-    color: 'white',
-    size:20
-};
+// hw2_trace2.text = hw2_trace2.y;
+// hw2_trace2.textfont = {
+//     color: 'white',
+//     size:20
+// };
 
 let hw2_trace3 ={};
 hw2_trace3.type ="bar";
-hw2_trace3.name ="第三產業（服務業）";
+hw2_trace3.name ="COVID-19";
 hw2_trace3.x =[];
 hw2_trace3.y =[];
 
-hw2_trace3.x[0] = "中國的職業人口分布";
-hw2_trace3.y[0] = 中國的職業人口[2]['count'];
-hw2_trace3.x[1] = "日本的職業人口分布";
-hw2_trace3.y[1] = 日本的職業人口[2]['count'];
-hw2_trace3.x[2] = "南韓的職業人口分布";
-hw2_trace3.y[2] = 南韓的職業人口[2]['count'];
-hw2_trace3.x[3] = "臺灣的職業人口分布";
-hw2_trace3.y[3] = 臺灣的職業人口[2]['count'];
+hw2_trace3.x[0] = "臺北市";
+hw2_trace3.y[0] = 臺北市死因統計[2]['count'];
+hw2_trace3.x[1] = "新北市";
+hw2_trace3.y[1] = 新北市死因統計[2]['count'];
+hw2_trace3.x[2] = "桃園市";
+hw2_trace3.y[2] = 桃園市死因統計[2]['count'];
+hw2_trace3.x[3] = "臺中市";
+hw2_trace3.y[3] = 臺中市死因統計[2]['count'];
+hw2_trace3.x[4] = "臺南市";
+hw2_trace3.y[4] = 臺南市死因統計[2]['count'];
+hw2_trace3.x[5] = "高雄市";
+hw2_trace3.y[5] = 高雄市死因統計[2]['count'];
 
-hw2_trace3.text = hw2_trace3.y;
-hw2_trace3.textfont = {
-    color: 'white',
-    size:20
-};
+// hw2_trace3.text = hw2_trace3.y;
+
+let hw2_trace4 ={};
+hw2_trace4.type ="bar";
+hw2_trace4.name ="肺炎";
+hw2_trace4.x =[];
+hw2_trace4.y =[];
+
+hw2_trace4.x[0] = "臺北市";
+hw2_trace4.y[0] = 臺北市死因統計[3]['count'];
+hw2_trace4.x[1] = "新北市";
+hw2_trace4.y[1] = 新北市死因統計[3]['count'];
+hw2_trace4.x[2] = "桃園市";
+hw2_trace4.y[2] = 桃園市死因統計[3]['count'];
+hw2_trace4.x[3] = "臺中市";
+hw2_trace4.y[3] = 臺中市死因統計[3]['count'];
+hw2_trace4.x[4] = "臺南市";
+hw2_trace4.y[4] = 臺南市死因統計[3]['count'];
+hw2_trace4.x[5] = "高雄市";
+hw2_trace4.y[5] = 高雄市死因統計[3]['count'];
+
+// hw2_trace4.text = hw2_trace4.y;
 //let trace2 ={};
 //trace2.type ="bar";
 //trace2.name ="Taoyuan Zoo";
@@ -89,11 +124,12 @@ let data2 =[];
 data2.push(hw2_trace1);
 data2.push(hw2_trace2);
 data2.push(hw2_trace3);
+data2.push(hw2_trace4);
 let layout2 ={
     margin:{
         t:50
     },
-    title:"臺灣鄰國職業人口分布",
+    title:"臺灣六都四大死因",
     barmode:"stack"
 };
 Plotly.newPlot(myGraph2, data2, layout2);
--- hw_3/main.js
+++ hw_3/main.js
@@ -5,6 +5,7 @@ hw3_trace1.type ="pie";
 hw3_trace1.title = "中國的人口年齡分布";
 hw3_trace1.labels =[];
 hw3_trace1.values =[];
+hw3_trace1.hole = 0.5;
 hw3_trace1.domain = {
     row: 0,
     column: 0
@@ -21,6 +22,7 @@ hw3_trace2.type ="pie";
 hw3_trace2.title ="日本的人口年齡分布" ;
 hw3_trace2.labels =[];
 hw3_trace2.values =[];
+hw3_trace2.hole = 0.5;
 hw3_trace2.domain = {
     row: 0,
     column: 1
@@ -37,6 +39,7 @@ hw3_trace3.type ="pie";
 hw3_trace3.title = "南韓的人口年齡分布";
 hw3_trace3.labels =[];
 hw3_trace3.values =[];
+hw3_trace3.hole = 0.5;
 hw3_trace3.domain = {
     row: 1,
     column: 0
@@ -53,6 +56,7 @@ hw3_trace4.type ="pie";
 hw3_trace4.title = "北韓的人口年齡分布";
 hw3_trace4.labels =[];
 hw3_trace4.values =[];
+hw3_trace4.hole = 0.5;
 hw3_trace4.domain = {
     row: 1,
     column: 1
@@ -69,6 +73,7 @@ hw3_trace5.type ="pie";
 hw3_trace5.title = "臺灣的人口年齡分布";
 hw3_trace5.labels =[];
 hw3_trace5.values =[];
+hw3_trace5.hole = 0.5;
 hw3_trace5.domain = {
     row: 0,
     column:2
